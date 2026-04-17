1f315c1f81dc03aff4043cd48e9096f1f846f4f1
Ekemini Sunday Ben <ekemiben.4@gmail.com>
2026-04-18 00:17:36 +0100

    updated port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
